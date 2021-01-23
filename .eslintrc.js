36d65f8f0ed98966bf5aa1fbba907a3c83cdc5da
viniciusgpereira <vini.gpereira2014@gmail.com>
2021-01-23 11:56:52 -0300

    chore: install and setup linter

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['prettier', 'airbnb-base'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
