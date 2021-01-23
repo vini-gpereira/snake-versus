2cea0b3d4d76d0f0c69ef1297af411e8e16bf8dc
viniciusgpereira <vini.gpereira2014@gmail.com>
2021-01-23 15:39:08 -0300

    chore: class to all game

---
+++ src/resources/Game/index.js
@@ -0,0 +1,50 @@
+import randint from '../../utils/generators';
+import { isPositionFree, isSamePlayer } from '../../utils/validators';
+
+class Game {
+  constructor({ gameSettings, actions, renderer }) {
+    this.boardSize = gameSettings.boardSize;
+    this.actions = actions;
+    this.renderer = renderer;
+    this.state = {
+      players: {},
+      foods: {},
+    };
+  }
+
+  buildInitialGame(players) {
+    players.forEach((player) => {
+      this.state.players[player] = this.randomFreePosition();
+    });
+  }
+
+  playerCommand(command) {
+    const { playerId, key } = command;
+    const player = this.state.players[playerId];
+    const newPlayer = this.actions.executeAction(player, key);
+
+    if (!isSamePlayer(player, newPlayer)) {
+      this.state.players[playerId] = newPlayer;
+    }
+  }
+
+  startGame() {}
+
+  randomFreePosition() {
+    const min = 0;
+    const max = this.boardSize - 1;
+    const { players, foods } = this.state;
+
+    let x = randint(min, max);
+    let y = randint(min, max);
+
+    while (!isPositionFree(x, y, players, foods)) {
+      x = randint(min, max);
+      y = randint(min, max);
+    }
+
+    return { x, y };
+  }
+}
+
+export default Game;
