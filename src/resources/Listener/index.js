e1c87c341106a9955c3913e851919b746ddb3a9a
viniciusgpereira <vini.gpereira2014@gmail.com>
2021-01-23 15:33:50 -0300

    chore: create class to listen inputs

---
+++ src/resources/Listener/index.js
@@ -0,0 +1,17 @@
+class Listener {
+  constructor({ gameSettings, watcher }) {
+    this.playerId = gameSettings.currentPlayerId;
+    this.watcher = watcher;
+  }
+
+  handleInput(event) {
+    const command = {
+      playerId: this.playerId,
+      key: event.key,
+    };
+
+    this.watcher.notify(command);
+  }
+}
+
+export default Listener;
