b8885a411135b4e344b9e4ab0555e0301964d11f
viniciusgpereira <vini.gpereira2014@gmail.com>
2021-01-24 18:33:21 -0300

    chore: remove useless validator

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
 export function isPositionFree(x, y, players, foods) {
   const hasPlayer = Object.values(players).find(
     (player) => player.x === x && player.y === y
@@ -12,35 +13,6 @@ export function isPositionFree(x, y, players, foods) {
   );
 
   if (hasFood) {
-    return false;
-  }
-
-  return true;
-}
-
-export function isSamePlayer(player1, player2) {
-  if (player1 === player2) {
-    return true;
-  }
-
-  if (!(player1 instanceof Object) || !(player2 instanceof Object)) {
-    return false;
-  }
-
-  if (Object.keys(player1).length !== Object.keys(player2).length) {
-    return false;
-  }
-
-  const hasDifferentValue = Object.keys(player1).find((key) => {
-    const valPlayer1 = player1[key];
-    const valPlayer2 = player2[key];
-
-    if (!valPlayer2 || valPlayer1 !== valPlayer2) {
-      return true;
-    }
-  });
-
-  if (hasDifferentValue) {
     return false;
   }
 
